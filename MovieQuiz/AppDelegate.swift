9d962649c102823149da4765aaa067c2d0833161
chtokaksam <denisshabalkin28@gmail.com>
2023-11-12 19:46:33 +0300

    В AppDelegate добавлен вывод на консоль для тестирования коммита через систему контроля версий.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,6 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        print("Hello World")
         return true
     }
 
@@ -24,3 +25,4 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {}
 }
+
